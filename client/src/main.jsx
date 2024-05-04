d54297305748b90b92d8f647a572252bfbe58012
Rafnas <101353809+Rafnas71@users.noreply.github.com>
2024-05-04 17:35:48 +0530

    Revert "last"
    This reverts commit 2b043500e4487a7a78f5f5a1884d320b9c87a8c8.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import { UserContextProvider } from './UserContext.jsx'
 import axios from 'axios'
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = import.meta.env.VITE_API__BASE_URL;
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
  
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
